c5e54b670636d0eb6e18bbb26b83bd9aa450428e
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-08-12 09:55:06 +0200

    update drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from "drizzle-kit";
+import "dotenv/config";
 
 export default {
   schema: "src/schema.ts",
   dialect: "postgresql",
   out: "drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
 } satisfies Config;
---
+++ readme.md
@@ -0,0 +1,20 @@
+## Getting started
+
+start a postgres db
+
+```shell
+docker-compose up -d
+```
+
+than run
+
+```shell
+pn db:migrate
+pn db:push
+```
+
+and run
+
+```shell
+pn dev
+```
